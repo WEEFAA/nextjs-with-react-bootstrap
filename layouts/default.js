50b3e933e8b03fe9d1ed85542d619dbb583e5d91
Ray Quijada <ray.quijada@jway.com>
2021-11-17 19:11:35 +0800

    [App] add default layout

---
+++ layouts/default.js
@@ -0,0 +1,26 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+
+const Layout = function (props) {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container className="justify-content-center">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/default_logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            NextJS with React-Bootstrap
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      { props.children }
+    </>
+  )
+}
+
+export default Layout
