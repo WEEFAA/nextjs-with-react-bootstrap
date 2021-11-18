33bd92e8b20fb54c5595dee7b6d2749e2baacc18
Ray Quijada <ray.quijada@jway.com>
2021-11-18 14:01:30 +0800

    [App] used alias imports & add bootstrap icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // Custom App
-import "./../styles/vendor.scss"
-import "./../styles/main.scss"
+import "@styles/vendor.scss"
+import "@styles/main.scss"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function App({ Component, pageProps }) {
   // Custom layout
